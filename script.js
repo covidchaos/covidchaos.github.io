756250b84f767a39ea4454c6274b0238d8e4dee3
Sundar Raman P <sundarramanp2000@gmail.com>
2020-04-19 15:23:49 +0530

    funic commit

--- script.js
+++ script.js
@@ -346,7 +346,7 @@ function Ball(posX, posY, velX, velY, r, healtimer, housetimer, hospitaltimer, c
                     infected=parseInt(population/2);
                 set=0;extra=0;extra2=0;
       			makeSim(population,fixedpopulation,lockedpopulation,infected);
-        		stateProxy.vaccines=parseInt(infected/2);		
+        		stateProxy.vaccines=parseInt(infected/2)+1;		
     			activateInterval();
     			sim.redraw();
     			}	
